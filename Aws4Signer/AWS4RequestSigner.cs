2682fef80f37be91b9e39ce1e554ce0a4314c44f
Michael Tsibelman <mtsibelman@csod.com>
2019-03-28 09:16:27 +0200

    Change escaping method and increase version

--- Aws4Signer/AWS4RequestSigner.cs
+++ Aws4Signer/AWS4RequestSigner.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Security.Cryptography;
 using System.Text;
@@ -96,7 +97,8 @@ namespace Aws4RequestSigner
 
             var canonical_request = new StringBuilder();
             canonical_request.Append(request.Method + "\n");
-            canonical_request.Append(string.Join("/", request.RequestUri.AbsolutePath.Split("/").Select(pathPart => WebUtility.UrlEncode(pathPart))) + "\n");
+           
+            canonical_request.Append(string.Join("/", request.RequestUri.AbsolutePath.Split('/').Select(Uri.EscapeDataString)) + "\n");
 
             var canonicalQueryParams = GetCanonicalQueryParams(request);
 
