da961dee9afb447a3d9831bb0742accec47ff3a9
rfestinger <rfestinger@csod.com>
2019-09-16 19:19:38 +0300

    finished fixes after code review

--- Aws4Signer/AWS4RequestSigner.cs
+++ Aws4Signer/AWS4RequestSigner.cs
@@ -7,12 +7,12 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 
-namespace Aws4RequestSigner
+namespace AWS4RequestSigner
 {
     public class AWS4RequestSigner
     {
-        private readonly string _access_key;
-        private readonly string _secret_key;
+        private readonly string _accessKey;
+        private readonly string _secretKey;
         private readonly SHA256 _sha256;
         private const string ALGORITHM = "AWS4-HMAC-SHA256";
         private const string EMPTY_STRING_HASH = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
@@ -30,8 +30,8 @@ namespace Aws4RequestSigner
                 throw new ArgumentOutOfRangeException(nameof(secretKey), secretKey, "Not a valid secret_key.");
             }
 
-            _access_key = accessKey;
-            _secret_key = secretKey;
+            _accessKey = accessKey;
+            _secretKey = secretKey;
             _sha256 = SHA256.Create();
         }
 
@@ -41,7 +41,7 @@ namespace Aws4RequestSigner
             return ToHexString(result);
         }
 
-        private static byte[] HmacSHA256(byte[] key, string data)
+        private static byte[] HmacSha256(byte[] key, string data)
         {
             var hashAlgorithm = new HMACSHA256(key);
 
@@ -50,18 +50,18 @@ namespace Aws4RequestSigner
 
         private static byte[] GetSignatureKey(string key, string dateStamp, string regionName, string serviceName)
         {
-            byte[] kSecret = Encoding.UTF8.GetBytes("AWS4" + key);
-            byte[] kDate = HmacSHA256(kSecret, dateStamp);
-            byte[] kRegion = HmacSHA256(kDate, regionName);
-            byte[] kService = HmacSHA256(kRegion, serviceName);
-            byte[] kSigning = HmacSHA256(kService, "aws4_request");
+            var kSecret = Encoding.UTF8.GetBytes("AWS4" + key);
+            var kDate = HmacSha256(kSecret, dateStamp);
+            var kRegion = HmacSha256(kDate, regionName);
+            var kService = HmacSha256(kRegion, serviceName);
+            var kSigning = HmacSha256(kService, "aws4_request");
             return kSigning;
         }
 
-        private static string ToHexString(byte[] array)
+        private static string ToHexString(IReadOnlyCollection<byte> array)
         {
-            var hex = new StringBuilder(array.Length * 2);
-            foreach (byte b in array)
+            var hex = new StringBuilder(array.Count * 2);
+            foreach (var b in array)
             {
                 hex.AppendFormat("{0:x2}", b);
             }
@@ -95,65 +95,64 @@ namespace Aws4RequestSigner
                 content = await request.Content.ReadAsByteArrayAsync();
             }
             
-            var payload_hash = EMPTY_STRING_HASH;
+            var payloadHash = EMPTY_STRING_HASH;
             if (content.Length != 0) {
-                request.Content = new ByteArrayContent(content);
-                payload_hash = Hash(content);
+                payloadHash = Hash(content);
             }
 
             if (request.Headers.Contains("x-amz-content-sha256") == false)
-                request.Headers.Add("x-amz-content-sha256", payload_hash);
+                request.Headers.Add("x-amz-content-sha256", payloadHash);
             
             var t = DateTimeOffset.UtcNow;
-            var amzdate = t.ToString("yyyyMMddTHHmmssZ");
-            request.Headers.Add("x-amz-date", amzdate);
-            var datestamp=t.ToString("yyyyMMdd");
+            var amzDate = t.ToString("yyyyMMddTHHmmssZ");
+            request.Headers.Add("x-amz-date", amzDate);
+            var dateStamp = t.ToString("yyyyMMdd");
 
-            var canonical_request = new StringBuilder();
-            canonical_request.Append(request.Method + "\n");
+            var canonicalRequest = new StringBuilder();
+            canonicalRequest.Append(request.Method + "\n");
            
-            canonical_request.Append(string.Join("/", request.RequestUri.AbsolutePath.Split('/').Select(Uri.EscapeDataString)) + "\n");
+            canonicalRequest.Append(string.Join("/", request.RequestUri.AbsolutePath.Split('/').Select(Uri.EscapeDataString)) + "\n");
 
             var canonicalQueryParams = GetCanonicalQueryParams(request);
 
-            canonical_request.Append(canonicalQueryParams + "\n");
+            canonicalRequest.Append(canonicalQueryParams + "\n");
 
             var signedHeadersList = new List<string>();
 
             foreach (var header in request.Headers.OrderBy(a => a.Key.ToLowerInvariant(), StringComparer.OrdinalIgnoreCase))
             {
-                canonical_request.Append(header.Key.ToLowerInvariant());
-                canonical_request.Append(":");
-                canonical_request.Append(string.Join(",", header.Value.Select(s => s.Trim())));
-                canonical_request.Append("\n");
+                canonicalRequest.Append(header.Key.ToLowerInvariant());
+                canonicalRequest.Append(":");
+                canonicalRequest.Append(string.Join(",", header.Value.Select(s => s.Trim())));
+                canonicalRequest.Append("\n");
                 signedHeadersList.Add(header.Key.ToLowerInvariant());
             }
 
-            canonical_request.Append("\n");
+            canonicalRequest.Append("\n");
 
-            var signed_headers = string.Join(";", signedHeadersList);
+            var signedHeaders = string.Join(";", signedHeadersList);
 
-            canonical_request.Append(signed_headers + "\n");
-            canonical_request.Append(payload_hash);
+            canonicalRequest.Append(signedHeaders + "\n");
+            canonicalRequest.Append(payloadHash);
             
-            var credential_scope = $"{datestamp}/{region}/{service}/aws4_request";
+            var credentialScope = $"{dateStamp }/{region}/{service}/aws4_request";
                        
-            var string_to_sign = $"{ALGORITHM}\n{amzdate}\n{credential_scope}\n" + Hash(Encoding.UTF8.GetBytes(canonical_request.ToString()));
+            var stringToSign = $"{ALGORITHM}\n{amzDate}\n{credentialScope}\n" + Hash(Encoding.UTF8.GetBytes(canonicalRequest.ToString()));
 
-            var signing_key = GetSignatureKey(_secret_key, datestamp, region, service);
-            var signature = ToHexString(HmacSHA256(signing_key, string_to_sign));
+            var signingKey = GetSignatureKey(_secretKey, dateStamp , region, service);
+            var signature = ToHexString(HmacSha256(signingKey, stringToSign));
             
-            request.Headers.TryAddWithoutValidation("Authorization", $"{ALGORITHM} Credential={_access_key}/{credential_scope}, SignedHeaders={signed_headers}, Signature={signature}");
+            request.Headers.TryAddWithoutValidation("Authorization", $"{ALGORITHM} Credential={_accessKey}/{credentialScope}, SignedHeaders={signedHeaders}, Signature={signature}");
 
             return request;
         }
 
         private static string GetCanonicalQueryParams(HttpRequestMessage request)
         {
-            SortedDictionary<string,string> values=new SortedDictionary<string, string>();
+            var values=new SortedDictionary<string, string>();
 
             var querystring = HttpUtility.ParseQueryString(request.RequestUri.Query);
-            foreach(string key in querystring.AllKeys)
+            foreach(var key in querystring.AllKeys)
             {
                 if (key == null)//Handles keys without values
                 {
